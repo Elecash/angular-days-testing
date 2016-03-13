89ff84934bb54e194bca02c44fe50cba0dd641a4
Raul <elecash@gmail.com>
2016-03-13 11:42:26 +0100

    Added config and custom locators file.

---
+++ tests/conf/e2e.conf.js
@@ -0,0 +1,39 @@
+exports.config = {
+    framework: 'jasmine2',
+    multiCapabilities: [
+        {
+            'browserName': 'chrome',
+            'chromeOptions': {
+                // Get rid of --ignore-certificate yellow warning
+                // args: ['--no-sandbox', '--test-type=browser'],
+
+                // Set download path and avoid prompting for download even though
+                // this is already the default on Chrome but for completeness
+                prefs: {
+                    'download': {
+                        'prompt_for_download': false,
+                        'default_directory': '/e2e/downloads/'
+                    }
+                }
+            }
+        }
+    ],
+    jasmineNodeOpts: {
+        // If true, print colors to the terminal.
+        showColors: true,
+
+        // Default time to wait in ms before a test fails.
+        defaultTimeoutInterval: 30000,
+
+        // Function called to print jasmine results.
+        // print: function() {},
+
+        // If set, only execute specs whose names match the pattern, which is
+        // internally compiled to a RegExp.
+        // grep: 'pattern',
+
+        // Inverts 'grep' matches
+        invertGrep: false
+    },
+    onPrepare: require('../e2e/custom-locators.e2e')
+};
