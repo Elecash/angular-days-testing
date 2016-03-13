42e61bd1df31bbfa717b414eacf78d880e9c8736
Raul <elecash@gmail.com>
2016-03-13 11:38:27 +0100

    Added GithubService tests.

--- scripts/services/github.js
+++ scripts/services/github.js
@@ -1,7 +1,7 @@
 angular.module('app')
     .service('GithubService', ['$resource',
         function($resource) {
-            // https://developer.github.com/v3/search/#search-users
+            // https://developer.github.com/v3/search?q=search
             this.getUsers = function(search) {
                 return $resource('https://api.github.com/search/users').get({q: search}).$promise;
             };
---
+++ tests/unit/services/github.spec.js
@@ -0,0 +1,23 @@
+describe('ui-view component', function() {
+    var $httpBackend;
+    var GithubService;
+
+    beforeEach(module('app'));
+
+    beforeEach(inject(function($injector) {
+        $httpBackend = $injector.get('$httpBackend');
+        GithubService = $injector.get('GithubService');
+    }));
+
+    it('Should call to https://api.github.com/search/users?q=elecash', function() {
+        $httpBackend.expectGET('https://api.github.com/search/users?q=elecash').respond(200, {user: 'elecash'});
+
+        GithubService.getUsers('elecash').then(
+            function(response) {
+                expect(response.user).toBe('elecash');
+            }
+        );
+
+        $httpBackend.flush();
+    });
+});
