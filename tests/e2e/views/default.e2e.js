33ce3c75e242c33f4a72221a0a939fc16d276550
Raul <elecash@gmail.com>
2016-03-13 16:23:12 +0100

    Added typeahead tests. Updated port and npm commands.

--- tests/e2e/views/default.e2e.js
+++ tests/e2e/views/default.e2e.js
@@ -1,6 +1,6 @@
 describe('Default route', function() {
     beforeEach(function() {
-        browser.get('http://localhost:9875/#/test');
+        browser.get('http://localhost:9877/#/test');
     });
 
     it('Should have an ui-view component', function() {
--- tests/e2e/views/ui-view.e2e.js
+++ tests/e2e/views/ui-view.e2e.js
@@ -1,9 +1,34 @@
 describe('View', function() {
     beforeEach(function() {
-        browser.get('http://localhost:9875/#/');
+        browser.get('http://localhost:9877/#/');
     });
 
     it('Should have an ui-view component', function() {
         expect(element(by.css('ui-view')).isPresent()).toBeTruthy();
+    });
+
+    it('Should show an empty dropdown if search field is empty', function() {
+        var search = element(by.model('ctrl.term'));
+        var results = element.all(by.repeater('item in ctrl.uiDataProvider'));
+        search.click();
+
+        expect(results.count()).toBe(0);
+    });
+
+    it('Should show several results in dropdown if search field is filled', function() {
+        var search = element(by.model('ctrl.term'));
+        var results = element.all(by.repeater('item in ctrl.uiDataProvider'));
+        search.sendKeys('angular');
+
+        expect(results.count()).toBeGreaterThan(0);
+    });
+
+    it('Should select firts result on press enter', function() {
+        var search = element(by.model('ctrl.term'));
+        var results = element.all(by.repeater('item in ctrl.uiDataProvider'));
+        search.sendKeys('elecash');
+        search.sendKeys(protractor.Key.ENTER);
+
+        expect(search.getAttribute('value')).toBe('Elecash');
     });
 });
